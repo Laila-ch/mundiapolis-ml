ed2b94776eeb69b6e5ab8ec0676783ed97a3772b
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-28 13:24:01 +0100

    Create 1-neuron.py

---
+++ 0x01-classification/1-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron: 
+    
+  def __init__(self,nx):
+    if type(nx)!="int": 
+      raise TypeError("nx must be an integer")
+    if nx<1 : 
+      raise ValueError("nx must be a positive integer")
+    
+    #Attributes
+    self.w = np.random.randn(2,2,2)
+    self.b = 0
+    self.A = 0
