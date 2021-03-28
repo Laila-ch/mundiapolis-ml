da9e2901e6d962c9aa20efd61f4f9646af7868f7
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-28 12:49:40 +0100

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -1,1 +1,13 @@
-
+#0-neuron.py
+class Neuron : 
+  def __init__(self,nx) :
+    if type(nx)!="int" : 
+      raise Exception("nx must be an integer")
+    if nx<1 : 
+      raise Exception("nx must be a positive integer")
+  w.self = np.random.normal(3, 2.5, size=(2, 4))
+  b.self = 0
+  A.self = 0
+  
+  
+      
