3eaef76898c4e9b71eee5c9eee6b9c8567ebd063
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-29 21:18:33 +0100

    Update 0-create_placeholders.py

--- 0x02-tensorflow/0-create_placeholders.py
+++ 0x02-tensorflow/0-create_placeholders.py
@@ -3,8 +3,11 @@
 import tensorflow as tf
 
 def create_placeholders(nx, classes):
+  #The placeholder for the input data to the neural network
   x = tf.placeholder(tf.float32, shape=[None,nx])
+  #The placeholder for the one-hot labels for the input data
   y = tf.placeholder(tf.float32, shape=[None,classes])
+  
   return x, y
 
 
