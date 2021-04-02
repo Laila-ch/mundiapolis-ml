8c5721cd591799d3bc01d1f8fe580ecdddab04a6
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-04-02 10:17:46 +0100

    Update 2-neuron.py

--- 0x01-classification/2-neuron.py
+++ 0x01-classification/2-neuron.py
@@ -4,28 +4,6 @@
 import numpy as np
 
 
-class Neuron: 
-    
-  def __init__(self,nx):
-    
-    if nx<1 : 
-      raise ValueError("nx must be a positive integer")
-    
-    #Attributes
-    self.W = np.random.randn(nx)
-    self.b = 0
-    self.A = 0
 
-@property
-def W(self):
-    return self.__W
-
-@property
-def b(self):
-    return self.__b
-
-@property
-def A(self):
-    return self.__A
     
     
