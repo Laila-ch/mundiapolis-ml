f2a47457147e9f21561598d84f1ded15b75f2c0e
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-04-16 23:28:09 +0000

    Update 1-create_layer.py

--- 0x02-tensorflow/1-create_layer.py
+++ 0x02-tensorflow/1-create_layer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
+
+
 import tensorflow as tf
 
+
 def create_layer(prev, n, activation):
+
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    model = tf.layers.Dense(units=n, activation=activation,
+                            name="layer", kernel_initializer=W)
+    return model(prev)
   
