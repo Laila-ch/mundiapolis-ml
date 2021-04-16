a67d9551515e2e555dc6f68bca8debfbb84b07ec
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-04-16 23:34:03 +0000

    Update 3-calculate_accuracy.py

--- 0x02-tensorflow/3-calculate_accuracy.py
+++ 0x02-tensorflow/3-calculate_accuracy.py
@@ -1,1 +1,12 @@
 
+#!/usr/bin/env python3
+
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+  
+    y_m = tf.argmax(y, axis=1)
+    yp_m = tf.argmax(y_pred, axis=1)
+    return tf.reduce_mean(tf.cast(tf.equal(y_m, yp_m), 'float32'))
