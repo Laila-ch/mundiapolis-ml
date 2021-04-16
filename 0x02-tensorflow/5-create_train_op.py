9b70fb30e9a068577ac66025eb4024aef1cfdb33
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-04-16 23:38:26 +0000

    Update 5-create_train_op.py

--- 0x02-tensorflow/5-create_train_op.py
+++ 0x02-tensorflow/5-create_train_op.py
@@ -1,1 +1,10 @@
 
+#!/usr/bin/env python3
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+
+    train = tf.train.GradientDescentOptimizer(alpha)
+    grads_and_vars = train.compute_gradients(loss)
+    return train.apply_gradients(grads_and_vars)
